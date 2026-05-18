08c43228f34a3dbb12b47d11adc37975192dc838
0308sue <b0107701@naver.com>
2026-05-18 14:33:54 +0900

    브랜치 테스트

--- back/src/main/java/com/example/back/BackApplication.java
+++ back/src/main/java/com/example/back/BackApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackApplication {
+    //테스트
 
     public static void main(String[] args) {
         SpringApplication.run(BackApplication.class, args);
